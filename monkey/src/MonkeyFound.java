36bd522b54c7448bc9b8a62b86089bd9012b9ff8
Tentacle-Monster <eigonofcarim@gmail.com>
2019-05-14 22:35:49 +0300

    comments to command line added

--- monkey/src/MonkeyFound.java
+++ monkey/src/MonkeyFound.java
@@ -10,18 +10,33 @@ public class MonkeyFound {
         Monkey vania = new Monkey(apple,mango);
         Monkey alfred = new Monkey(apple,banana,mango);
         Random random = new Random();
+        System.out.println("there are 4 monkeys:" +
+                "\n > Vasia eats nothing" +
+                "\n > Ergan eats mango" +
+                "\n > Vania eats apple and mango" +
+                "\n > Alfred eats apple, banana and mango" );
         FruitTree mangotree = new FruitTree(mango,random);
         FruitTree bananatree1 = new FruitTree(banana,random);
         FruitTree bananatree2 = new FruitTree(banana,random);
         FruitTree appletree = new FruitTree(apple,random);
+        System.out.println("Alfred & banana tree");
         System.out.println(alfred.search(bananatree1));
+        System.out.println("Vania & banana tree");
         System.out.println(vania.search(bananatree1));
+        System.out.println("Alfred & second banana tree");
         System.out.println(alfred.search(bananatree2));
+        System.out.println("Alfred & mango tree");
         System.out.println(alfred.search(mangotree));
+        System.out.println("Ergan & banana tree");
         System.out.println(ergan.search(mangotree));
+        System.out.println("Vania & banana tree");
         System.out.println(vania.search(mangotree));
+        System.out.println("Vasia & apple tree");
         System.out.println(vasia.search(appletree));
+        System.out.println("well, NullPointer exception");
+        System.out.println("now, we should add apples to Vasia's ration");
         vasia.addToRation(apple);
+        System.out.println("Vasia & apple tree, second try");
         System.out.println(vania.search(appletree));
 
 
